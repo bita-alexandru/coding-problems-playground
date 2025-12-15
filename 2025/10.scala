c0dc662a03800d38369076e6f75840030b021ba1
Alexandru Bita <bita.alexandru24@gmail.com>
2025-12-15 18:29:03 +0200

    rework day10

--- 2025/10.scala
+++ 2025/10.scala
@@ -8,110 +8,64 @@ def input(isExample: Boolean) =
             val parts = line.split(" ").toSeq
             val targetMask = parts.head.drop(1).dropRight(1).zipWithIndex.foldLeft(0L): 
                 case (acc, (c, i)) => if c == '#' then acc | (1L << i) else acc
-            val buttonMasks = parts.slice(1, parts.length - 1).map: schema =>
-                schema.drop(1).dropRight(1).split(",").foldLeft(0L): (acc, s) =>
-                    acc | (1L << s.toInt)
-            val targets = parts.last.drop(1).dropRight(1).split(",").map(_.trim.toInt).toSeq
-            val buttons = parts.slice(1, parts.length - 1).map: schema =>
-                val content = schema.drop(1).dropRight(1)
-                val vec = Array.fill(targets.length)(0)
-                if content.nonEmpty then
-                    content.split(",").map(_.trim.toInt).foreach: idx =>
-                        if idx < vec.length then vec(idx) = 1
-                vec.toSeq
-            (targetMask, buttonMasks, (targets, buttons))
+            val buttonIndices = parts.slice(1, parts.length - 1).map: schema =>
+                schema.drop(1).dropRight(1).split(",").map(_.toInt)
+            val buttonMasks = buttonIndices.map(_.foldLeft(0L)((acc, n) => acc | (1L << n)))
+            val buttons = buttonIndices.zip(buttonMasks).map(button => (indices = button._1, mask = button._2))
+            val joltages = parts.last.drop(1).dropRight(1).split(",").map(_.trim.toInt).toSeq
+            (targets = targetMask, buttons = buttons, joltages = joltages)
         .toSeq
 end input
 
 def part1(isExample: Boolean = false) =
     input(isExample).map: (target, buttons, _) =>
         (1 to buttons.length).find: k => 
-            buttons.combinations(k).exists(_.foldLeft(0L)(_ ^ _) == target)
+            buttons.map(_.mask).combinations(k).exists(_.foldLeft(0L)(_ ^ _) == target)
         .getOrElse(0)
     .sum
 end part1
 
 def part2(isExample: Boolean = false) =
-    def solveSystem(target: Seq[Int], buttons: Seq[Seq[Int]]) =
-        val rows = target.length
-        val cols = buttons.length
-        val matrix = Array.ofDim[Double](rows, cols + 1)
-        
-        for c <- 0 until cols do
-            for r <- 0 until rows do
-                matrix(r)(c) = buttons(c)(r).toDouble
-        for r <- 0 until rows do
-            matrix(r)(cols) = target(r).toDouble
+    val INF = 1_000_000L
 
-        var pivotRow = 0
-        val pivotCols = scala.collection.mutable.ArrayBuffer[Int]()
+    input(isExample).map:
+        case (_, buttons, joltages) =>
+            val buttonMasks = buttons.map(_.mask)
+            val buttonIndices = buttons.map(_.indices)
 
-        for col <- 0 until cols if pivotRow < rows do
-            var sel = pivotRow
-            while sel < rows && matrix(sel)(col).abs < 1e-9 do
-                sel += 1
-            
-            if sel < rows then
-                val tmp = matrix(pivotRow)
-                matrix(pivotRow) = matrix(sel)
-                matrix(sel) = tmp
+            val n = joltages.length
+            val allSubsets =
+                (0 to buttonMasks.length).flatMap: k =>
+                    buttonMasks.indices.combinations(k).map: idxs =>
+                        val xorMask = idxs.foldLeft(0L)((acc, i) => acc ^ buttonMasks(i))
+                        val delta = Array.fill(n)(0)
+                        idxs.foreach(i => buttonIndices(i).foreach(delta(_) += 1))
+                        (xorMask, delta.toSeq, k)
+            .toSeq
 
-                val div = matrix(pivotRow)(col)
-                for c2 <- col to cols do matrix(pivotRow)(c2) /= div
-
-                for r <- 0 until rows if r != pivotRow do
-                    val factor = matrix(r)(col)
-                    if factor.abs > 1e-9 then
-                        for c2 <- col to cols do matrix(r)(c2) -= factor * matrix(pivotRow)(c2)
-
-                pivotCols += col
-                pivotRow += 1
-
-        val freeCols = (0 until cols).filterNot(pivotCols.contains).toArray
-        val bounds = freeCols.map: c =>
-            val relevantTargets = (0 until rows).collect:
-                case r if buttons(c)(r) >= 1 => target(r)
-            if relevantTargets.isEmpty then 0 else relevantTargets.min
-
-        var minPresses = Long.MaxValue
-        def iterate(idx: Int, currentFreeVals: Array[Int]): Unit =
-            if idx == freeCols.length then
-                var currentSum = 0L
-                var isValid = true
-                var rowIdx = 0
-                val pivotIt = pivotCols.iterator
-                while isValid && pivotIt.hasNext do
-                    val pCol = pivotIt.next()
-                    var value = matrix(rowIdx)(cols)
-
-                    var f = 0
-                    while f < freeCols.length do
-                        val fCol = freeCols(f)
-                        value -= matrix(rowIdx)(fCol) * currentFreeVals(f)
-                        f += 1
-                    
-                    if value < -1e-5 || (value.round - value).abs > 1e-5 then
-                        isValid = false
+            val memo = scala.collection.mutable.Map[Seq[Int], Long]()
+            def f(v: Seq[Int]): Long =
+                memo.getOrElseUpdate(v, {
+                    if v.forall(_ == 0) then 0L
+                    else if v.exists(_ < 0) then INF
                     else
-                        val intVal = value.round.toInt
-                        currentSum += intVal
-                    
-                    rowIdx += 1
-                if isValid then
-                    currentFreeVals.foreach(v => currentSum += v)
-                    minPresses = currentSum.min(minPresses)
-            else
-                val c = freeCols(idx)
-                for v <- 0 to bounds(idx) do
-                    iterate(idx + 1, currentFreeVals :+ v)
-        end iterate
-        iterate(0, Array())
+                        val target = v.zipWithIndex.foldLeft(0L):
+                            case (acc, (x, i)) => if (x & 1) == 1 then acc | (1L << i) else acc
+                        var best = INF
 
-        minPresses
-    end solveSystem
+                        allSubsets.foreach:
+                            case (mask, delta, k) =>
+                                if mask == target then
+                                    val reduced = v.indices.map(i => v(i) - delta(i))
+                                    if reduced.forall(x => x >= 0 && (x & 1) == 0) then
+                                        val next = reduced.map(_ >> 1)
+                                        best = math.min(best, 2 * f(next) + k)
+                        best
+                    end if
+                })
+            end f
 
-    input(isExample).map: (_, _, data) =>
-        solveSystem(data._1, data._2)
+            f(joltages)
     .sum
 end part2
 
